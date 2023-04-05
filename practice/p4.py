721588405fffed7c43181455a6f7e1a5194d2b36
merey <m_kemelbay@kbtu.kz>
2023-04-05 22:02:17 +0600

    lab_8

---
+++ practice/p4.py
@@ -0,0 +1,7 @@
+import pygame
+#from you import something specific
+#import you import everything fri=om the module
+#if i don't use the clock it will be too fast
+#bracet after class is inheritance
+#update is used for to change some specific space
+#
